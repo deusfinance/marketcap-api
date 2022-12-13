62e9758164b67fb341bfb5acc1ca538adb0c40dd
Hassan Abbasi <hassan.abbp@gmail.com>
2022-12-13 20:20:25 +0330

    Add route for single chain request

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 from configs import chain_configs, redis_prefix
 from redis_client import redis_client
@@ -6,9 +6,18 @@ from redis_client import redis_client
 app = Flask(__name__)
 
 
-@app.route("/get-market-caps")
+@app.route("/getMarketCaps")
 def get_market_caps():
     res = {}
     for chain, config in chain_configs.items():
         res[chain] = int(redis_client.get(redis_prefix + chain))
     return res
+
+
+@app.route("/marketCap/<chain>")
+def get_market_caps(chain):
+    if chain.lower() not in chain_configs:
+        return jsonify(status='error', msg=f'Invalid chain: `{chain}`'), 400
+
+    market_cap = int(redis_client.get(redis_prefix + chain.lower()))
+    return jsonify(status='ok', chain=chain.lower(), marketCap=market_cap), 200
