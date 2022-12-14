e5a76bf795dbd6d8a6319fec61f96a7407d858d0
Hassan Abbasi <hassan.abbp@gmail.com>
2022-12-14 19:04:28 +0330

    Change getMarketCap route

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 
 from constants import non_circulating_contracts
-from config import PRICE_REDIS_TAG, REDIS_PREFIX
+from config import PRICE_REDIS_TAG, NC_SUPPLY_REDIS_PREFIX, TOTAL_SUPPLY_REDIS_PREFIX
 from redis_client import redis_client
 
 chains = list(non_circulating_contracts)
@@ -11,17 +11,34 @@ app = Flask(__name__)
 @app.route('/getMarketCap')
 def get_market_cap():
     price = float(redis_client.get(PRICE_REDIS_TAG))
-    res = {}
+    result = {}
+    totalAmounts = dict(totalSupply=0,
+                        circulatingSupply=0,
+                        totalMarketCap=0,
+                        circulatingMarketCap=0)
     for chain in chains:
-        res[chain] = round(int(redis_client.get(REDIS_PREFIX + chain)) * price)
-    return res
+        supply = int(redis_client.get(TOTAL_SUPPLY_REDIS_PREFIX + chain))
+        circulating_supply = supply - int(redis_client.get(NC_SUPPLY_REDIS_PREFIX + chain))
+        market_cap = round(supply * price / 1e18)
+        circulating_market_cap = round(circulating_supply * price / 1e18)
+        result[chain] = dict(totalSupply=supply,
+                             circulatingSupply=circulating_supply,
+                             totalMarketCap=market_cap,
+                             circulatingMarketCap=circulating_market_cap)
+        totalAmounts['totalSupply'] += supply
+        totalAmounts['circulatingSupply'] += circulating_supply
+        totalAmounts['totalMarketCap'] += market_cap
+        totalAmounts['circulatingMarketCap'] += circulating_market_cap
+
+    result['total'] = totalAmounts
+    return result
 
 
 @app.route('/getCirculatingSupplies')
 def get_circulating_supplies():
     res = {}
     for chain in chains:
-        res[chain] = int(redis_client.get(REDIS_PREFIX + chain))
+        res[chain] = int(redis_client.get(NC_SUPPLY_REDIS_PREFIX + chain))
     return res
 
 
@@ -30,5 +47,5 @@ def get_circulating_supply(chain):
     if chain.lower() not in chains:
         return jsonify(status='error', msg=f'Invalid chain: `{chain}`'), 400
 
-    market_cap = int(redis_client.get(REDIS_PREFIX + chain.lower()))
+    market_cap = int(redis_client.get(NC_SUPPLY_REDIS_PREFIX + chain.lower()))
     return jsonify(status='ok', chain=chain.lower(), marketCap=market_cap), 200
--- updater.py
+++ updater.py
@@ -1,6 +1,6 @@
 import time
 
-from config import update_timeout, REDIS_PREFIX, PRICE_REDIS_TAG
+from config import update_timeout, NC_SUPPLY_REDIS_PREFIX, PRICE_REDIS_TAG, TOTAL_SUPPLY_REDIS_PREFIX
 from constants import non_circulating_contracts
 from redis_client import redis_client
 
@@ -16,22 +16,28 @@ def run_updator():
         for chain, contracts in non_circulating_contracts.items():
             deus_contract = managers[chain].deus_contract
             mc = managers[chain].mc
-            print('Checking ' + chain)
+            print(f'{chain:.^40}')
             try:
                 total_supply = deus_contract.functions.totalSupply().call()
                 if contracts:
-                    nc_supply = sum(balance[0] for balance in mc.balanceOf(contracts.values()))
-                    total_supply -= nc_supply
-                print(total_supply)
-                redis_client.set(REDIS_PREFIX + chain, total_supply)
+                    nc_supply = sum(balance[0] for balance in mc.balanceOf(set(contracts.values())))
+                else:
+                    nc_supply = 0
+                redis_client.set(NC_SUPPLY_REDIS_PREFIX + chain, nc_supply)
+                redis_client.set(TOTAL_SUPPLY_REDIS_PREFIX + chain, total_supply)
             except Exception as ex:
                 print('Error:', ex)
                 managers[chain].update_rpc()
-
+            else:
+                print('TOTAL SUPPLY:   ', total_supply)
+                print('NON-CIRCULATING:', nc_supply)
         try:
-            redis_client.set(PRICE_REDIS_TAG, str(deus_spooky()))
+            price = str(deus_spooky())
+            redis_client.set(PRICE_REDIS_TAG, price)
         except Exception as ex:
             print('Error:', ex)
+        else:
+            print(f'\n*** DEUS PRICE ***\n{price}\n')
 
         time.sleep(update_timeout)
 
