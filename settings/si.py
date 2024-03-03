86d3484923050efa22b3e51b152fbbc2598cf65c
Hassan Abbasi <hassan.abbp@gmail.com>
2024-03-03 19:15:48 +0330

    Update excludes

--- settings/si.py
+++ settings/si.py
@@ -44,6 +44,10 @@ class Network:
                     '0x0092fc463b969347f2F6d18a572BDf99F61B5e8F',  # Manifold
                     '0x6E0098A8c651F7A6A9510B270CD02c858C344D94',  # BuyBacker
                     '0x83B285E802D76055169B1C5e3bF21702B85b89Cb',  # Lafa
+                    '0x77eeaf07C050a690f9B3C2E8e7642Cc3CBEcEEb4',  # RossBribingWallet
+                    '0x04063BE551982933c2F512A0f116D6ECc445F8eC',  # DeusOps
+                    '0xBB901d8D3fAF0b675e443B2DE743d149bfe68353',  # ClaimDeus contract
+                    '0x6c9E3B6b6C528ffdF0b5248a2B47069fcEc9e835',  # DeusConversion contract
                 ],
             }
         elif name == Network.MAINNET:
@@ -59,6 +63,8 @@ class Network:
                     '0x34424Fff5E91d7CE07E82d2e9c8A1a02fA4D0cF2',  # veSOLID escrow
                     '0xf3f5E899059728e0c8aADd38779720549cf37a18',  # anyDEUS (bridge)
                     '0x83B285E802D76055169B1C5e3bF21702B85b89Cb',  # Lafa
+                    '0x77eeaf07C050a690f9B3C2E8e7642Cc3CBEcEEb4',  # RossBribingWallet
+                    '0x04063BE551982933c2F512A0f116D6ECc445F8eC',  # DeusOps
                 ],
             }
         elif name == Network.ARBITRUM:
@@ -77,6 +83,8 @@ class Network:
                     '0x42d05d13F951AA7c35Cc14453D594427928bF898',  # FeeDistributor
                     '0x0cF9Ce01b99A62B1c2AE7A79370aF5f8518dfa9d',  # BribeContract
                     '0x83B285E802D76055169B1C5e3bF21702B85b89Cb',  # Lafa
+                    '0x77eeaf07C050a690f9B3C2E8e7642Cc3CBEcEEb4',  # RossBribingWallet
+                    '0x04063BE551982933c2F512A0f116D6ECc445F8eC',  # DeusOps
                 ],
             }
         elif name == Network.POLYGON:
@@ -87,6 +95,7 @@ class Network:
                     '0xe3b6CC7b76a7f67BBCcb66c010780bE0AF31Ff05',  # Migrator
                     '0x77eeaf07C050a690f9B3C2E8e7642Cc3CBEcEEb4',  # RossBribingWallet
                     '0x83B285E802D76055169B1C5e3bF21702B85b89Cb',  # Lafa
+                    '0x04063BE551982933c2F512A0f116D6ECc445F8eC',  # DeusOps
                 ],
             }
         elif name == Network.BSC:
@@ -95,12 +104,13 @@ class Network:
                     '0xe3b6CC7b76a7f67BBCcb66c010780bE0AF31Ff05',  # Migrator
                     '0xE5227F141575DcE74721f4A9bE2D7D636F923044',  # BridgeWallet (Lafa)
                     '0x6a524C7328eb652248d1a3786f9DB0e74CA961F0',  # ADMIN
-                    '0x77eeaf07C050a690f9B3C2E8e7642Cc3CBEcEEb4',  # ThenaBribe
+                    '0x77eeaf07C050a690f9B3C2E8e7642Cc3CBEcEEb4',  # RossBribingWallet
                     '0xd1604f00F0101c87047cf7E892f04998FB1AE437',  # Bribe
                     '0x50437C8AD1fC8c40BF361d531a4f0eD215175eA1',  # ThenaBribeContract
                     '0x95fB54D8B17c8f7e94DA3aCb8D6cd11FAC29a969',  # ThenaBribeContract
                     '0xaB1Ad7FA79508ac913d3C30EA8952a4486a74451',  # ThenaBribeContract
                     '0x83B285E802D76055169B1C5e3bF21702B85b89Cb',  # Lafa
+                    '0x04063BE551982933c2F512A0f116D6ECc445F8eC',  # DeusOps
                 ],
             }
         elif name == Network.KAVA:
@@ -109,6 +119,8 @@ class Network:
                     '0xE5227F141575DcE74721f4A9bE2D7D636F923044',  # * non circulating
                     '0x8c352C7e3559390EB7e9B84b291997a89A5abc93',  # * migrated
                     '0x83B285E802D76055169B1C5e3bF21702B85b89Cb',  # Lafa
+                    '0x77eeaf07C050a690f9B3C2E8e7642Cc3CBEcEEb4',  # RossBribingWallet
+                    '0x04063BE551982933c2F512A0f116D6ECc445F8eC',  # DeusOps
                 ],
             }
         elif name == Network.AVAX:
@@ -121,6 +133,8 @@ class Network:
                     '0x4a0A3902e091cdb3AEc4279a6BFAC50297F0A79e',  # WHALE
                     '0x83B4678A24955A4100bad18f34dE65D03C711f4C',  # ExternalBribe
                     '0x83B285E802D76055169B1C5e3bF21702B85b89Cb',  # Lafa
+                    '0x77eeaf07C050a690f9B3C2E8e7642Cc3CBEcEEb4',  # RossBribingWallet
+                    '0x04063BE551982933c2F512A0f116D6ECc445F8eC',  # DeusOps
                 ],
             }
         elif name == Network.BASE:
@@ -131,6 +145,8 @@ class Network:
                     '0x83B285E802D76055169B1C5e3bF21702B85b89Cb',  # Lafa
                     '0xcf66F70B7d88749C1Fd2c4287Dc637ca24BA3AF2',  # BribeVotingReward
                     '0x83B285E802D76055169B1C5e3bF21702B85b89Cb',  # Lafa
+                    '0x77eeaf07C050a690f9B3C2E8e7642Cc3CBEcEEb4',  # RossBribingWallet
+                    '0x04063BE551982933c2F512A0f116D6ECc445F8eC',  # DeusOps
                 ],
             }
         else:
